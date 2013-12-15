3bd04f9dc53fedb55a28be4206ded6c49e6d499c
Kenneth Lu <krlu91@gmail.com>
2013-12-14 23:48:32 -0500

    add worldMap file

---
+++ myRPG/src/RPGelements/WorldMap.java
@@ -0,0 +1,14 @@
+package RPGelements;
+import FundamentalStructures.Tuple;
+
+public class WorldMap {
+	
+	protected int[] grid;
+	
+	/*returns position of input user 
+	 * TODO: This doesn't do anything yet!
+	 */
+	public Tuple<Integer,Integer>getPosition(CharacterProfile profile){
+		return null;
+	}
+}
