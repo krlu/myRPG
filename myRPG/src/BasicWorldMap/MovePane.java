b1b1e254ae5cbe1b3a87ab368ed636dfba14bb9b
Kenneth Lu <krlu91@gmail.com>
2014-11-05 14:37:08 -0500

    trying to add mouse dragging feature

--- myRPG/src/BasicWorldMap/MovePane.java
+++ myRPG/src/BasicWorldMap/MovePane.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
@@ -239,21 +240,21 @@ public class MovePane {
         }
         
         public void addMouseListener(JPanel pool, final CharacterProfile profile){
-
-        	pool.addMouseListener(new MouseListener(){
-            	private int X = 0; 
+        /*	pool.addMouseMotionListener(new MouseMotionListener(){
+        		
+        		private int X = 0; 
             	private int Y = 2*barHeight;
             	private JPanel avatar;
             	private Rectangle r;
             	private int SPEED = profile.getMovementSpeed();
             	private int usedBlink = 0;
-            	private boolean mouseHeld = false;
             	
             	// TODO: find less hacky solution to creating timers!!
             	private Timer mouseTimer = new Timer(10, new MyActionListener(profile){
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
+						//System.out.println(mouseHeld + "  ");
 						Container parent = mobby.getParent();
 						Tuple<Double,Double> vector = vector2D(X-r.x,Y-r.y);
 						if(profile.getCurrentHp() == 0){
@@ -352,6 +353,136 @@ public class MovePane {
 					
             	});	
 				@Override
+				public void mouseDragged(MouseEvent e) {
+					avatar = profile.avatar;
+					r = avatar.getBounds(); 
+					X = e.getX() - r.width/2; 
+					Y = e.getY() - r.height/2;					
+				}
+
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					avatar = profile.avatar;
+					r = avatar.getBounds(); 
+					mouseTimer.start();				
+				}
+				public Tuple<Double, Double> vector2D(int xDiff, int yDiff){
+					
+					double magnitude = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff,2));					
+					if(magnitude == 0){
+						return new Tuple<Double,Double>(0.0,0.0);
+					}
+					Tuple<Double, Double> vector2D = new Tuple<Double, Double>(xDiff/magnitude,yDiff/magnitude);
+					return vector2D;
+				}
+        		
+        	});*/
+        	pool.addMouseListener(new MouseListener(){
+            	private int X = 0; 
+            	private int Y = 2*barHeight;
+            	private JPanel avatar;
+            	private Rectangle r;
+            	private int SPEED = profile.getMovementSpeed();
+            	private int usedBlink = 0;
+            	
+            	// TODO: find less hacky solution to creating timers!!
+            	private Timer mouseTimer = new Timer(10, new MyActionListener(profile){
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						//System.out.println(mouseHeld + "  ");
+						Container parent = mobby.getParent();
+						Tuple<Double,Double> vector = vector2D(X-r.x,Y-r.y);
+						if(profile.getCurrentHp() == 0){
+							mouseTimer.stop();
+						}				
+						int xDist = Math.abs(X - r.x);
+						int yDist = Math.abs(Y - r.y);
+						
+						// for when vector components are < 1
+						if(xDist == 1){
+							r.x = X;							
+						}
+						else if(r.x > X){
+							r.x += Math.min(xDist,SPEED)*vector.l;
+							profile.setOrientation(Direction.Left);
+						}
+						else if(r.x < X){
+							r.x += Math.min(xDist,SPEED)*vector.l;
+							profile.setOrientation(Direction.Right);
+						}
+
+						
+						if(yDist == 1){
+							r.y = Y;
+						}
+						else if(r.y > Y){
+							r.y += Math.min(yDist,SPEED)*vector.r;
+							profile.setOrientation(Direction.Up);
+						}
+						else if(r.y < Y){
+							r.y += Math.min(yDist,SPEED)*vector.r;
+							profile.setOrientation(Direction.Down);
+						}
+						switch (moveDirection) {
+	                        case Blink:	                        	 
+	                        	 // TODO: no other targets right now!!! only self targeting!!
+	                        	 usedBlink = movementSkills.get(0).applyTargetedEffect(profile, null); 
+	                        	 if(usedBlink > 0){
+	                        		 X = profile.getCoordinatePosition().l;
+	                        		 Y = profile.getCoordinatePosition().r;
+		                        	 r.x = profile.getCoordinatePosition().l;
+		                        	 r.y = profile.getCoordinatePosition().r; 							 	
+	                        	 }
+	                        default:
+	                        	break;
+	                    }
+						if (r.x < 0) {
+	                        r.x = 0;
+	                        X = r.x;
+	                        mobby.setBackground(randomColor());
+	                        profile.updateHp(-10);
+	                    } else if (r.x + r.width > parent.getWidth()) {
+	                    	mobby.setBackground(randomColor());
+	                        r.x = parent.getWidth() - r.width;
+	                        X = r.x;
+	                        profile.updateHp(-10);
+	                    }
+	                    if (r.y < 2*barHeight) {
+	                    	mobby.setBackground(randomColor());
+	                        r.y = 2*barHeight;
+	                        profile.updateHp(-10);
+	                        Y = r.y;
+	                    } else if (r.y + r.height > parent.getHeight()) {
+	                    	mobby.setBackground(randomColor());
+	                        r.y = parent.getHeight() - r.height;
+	                        profile.updateHp(-10);
+	                        Y = r.y;
+	                    }
+	                    profile.setPosition(r.x, r.y);
+	                    mobby.setBounds(r);
+
+						avatar.setBounds(r);
+					}
+					
+					 public Color randomColor(){
+		                	double i = Math.random();
+		                	if(i < 0.25){
+		                		return Color.GREEN;
+		                	}
+		                	else if(i < 0.5){
+		                		return Color.BLUE;
+		                	}
+		                	else if (i < 0.75){
+		                		return Color.RED;
+		                	}
+		                	else{
+		                		return Color.ORANGE;
+		                	}
+		                }
+					
+            	});	
+				@Override
 				public void mouseClicked(MouseEvent e) {
 				}
 
@@ -373,12 +504,11 @@ public class MovePane {
 					X = e.getX() - r.width/2; 
 					Y = e.getY() - r.height/2;
 					mouseTimer.start();
-					mouseHeld = true;
+
 				}
 
 				@Override
 				public void mouseReleased(MouseEvent e) {		
-					mouseHeld = false;
 				}
 				
 				public Tuple<Double, Double> vector2D(int xDiff, int yDiff){
